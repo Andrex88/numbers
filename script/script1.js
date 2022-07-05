4e269a5577bd3d4c819b6e9fd05f57980a76e749
cristian palacio <crpalacio52@gmail.com>
2022-07-04 23:50:45 -0500

    update toGrid

--- script/script1.js
+++ script/script1.js
@@ -1,42 +1,47 @@
 import { NumeroALetras } from "./NumeroALetras.js";
 
 function addTable(num) {
-    document.getElementById('numeroX').textContent = num;
-    speechSynthesis.cancel();
-    var table = document.getElementById("myDynamicTable");
-    var elmtTable = table;
-    var tableRows = elmtTable.getElementsByTagName('tr');
-    var rowCount = tableRows.length;
-    
-    for (var x=rowCount-1; x>0; x--) {
-       elmtTable.removeChild(tableRows[x]);
-    }
+  document.getElementById('numeroX').textContent = num;
+  speechSynthesis.cancel();
+  var table = document.getElementById("contar");
+  var elmtTable = table;
+  var tableRows = elmtTable.getElementsByTagName('tr');
+  var rowCount = tableRows.length;
 
-    for (var i = 1; i <= num; i++) {
-      var tr = document.createElement('TR');
-      table.appendChild(tr);
-      
-      var td = document.createElement('TD');
-      td.width = '75';
-      td.appendChild(document.createTextNode(i));
-      tr.appendChild(td);
-
-      var td = document.createElement('TD');
-      td.width = 'auto';
-      td.appendChild(document.createTextNode(NumeroALetras(i)));
-      tr.appendChild(td);
-      decir(NumeroALetras(i));
-           
-    }
+  for (var x = rowCount - 1; x > 0; x--) {
+    elmtTable.removeChild(tableRows[x]);
   }
-  
-document.getElementById('hablar').addEventListener("click",()=>{
-    decir(document.getElementById("texto").value);
+  document.getElementById("contar").innerHTML = "";
+
+  for (var i = 1; i <= num; i++) {
+    // var tr = document.createElement('TR');
+    // table.appendChild(tr);
+
+    const td = document.createElement('p');
+    td.className = "caja";
+    const str = i.toString().concat(" - ").concat(NumeroALetras(i));
+    td.appendChild(document.createTextNode(str));
+    const number = i;
+    table.appendChild(td);
+    td.addEventListener("click", () => {
+      decir(NumeroALetras(number));
+    });
+    // var td = document.createElement('p');
+    // td.className="caja";
+    // td.appendChild();
+    // table.appendChild(td);
+    // decir(NumeroALetras(i));
+
+  }
+}
+
+document.getElementById('hablar').addEventListener("click", () => {
+  decir(document.getElementById("texto").value);
 });
-document.getElementById('ingresoNumero').addEventListener("click",()=>{
+document.getElementById('ingresoNumero').addEventListener("click", () => {
   addTable(document.getElementById("numero").value);
 });
 
-function decir(texto){
-    speechSynthesis.speak(new SpeechSynthesisUtterance(texto));
+function decir(texto) {
+  speechSynthesis.speak(new SpeechSynthesisUtterance(texto));
 }
