7ba22d59bac35eb229d5c6c644edf34665b69c7a
cristian palacio <crpalacio52@gmail.com>
2022-07-30 23:26:15 -0500

    update numbers

--- script/script1.js
+++ script/script1.js
@@ -1,26 +1,49 @@
 import { NumeroALetras } from "./NumeroALetras.js";
 
-function llenarNumeros(num) {
-  document.getElementById('numeroY').textContent = num;
-  speechSynthesis.cancel();
-  var contarId = document.getElementById("contar");
-  contarId.innerHTML = "";
+async function llenarNumeros(num) {
 
-  for (var i = 1; i <= num; i++) {
+  document.getElementById("numeroY").textContent = num;
+    speechSynthesis.cancel();
+    var contarId = document.getElementById("contar");
+    contarId.innerHTML = "";
+    let finalizo = false;
 
-    const elementCaja = document.createElement('p');
-    elementCaja.className = "caja";
-    let str = i.toString();
-    if(window.matchMedia("(min-width: 627px)").matches){
-      str = str.concat(" - ").concat(NumeroALetras(i))
+    setTimeout(() => {
+      document.getElementById("loading").style.display = "flex";
+
+    for (let i = 1; i <= num; i++) {
+      const elementCaja = document.createElement("p");
+      elementCaja.className = "caja";
+      let str = i.toString();
+
+      if (window.matchMedia("(min-width: 627px)").matches) {
+        str = str.concat(" - ").concat(NumeroALetras(i));
+      }
+
+      elementCaja.appendChild(document.createTextNode(str));
+      const number = i;
+      contarId.appendChild(elementCaja);
+
+      elementCaja.addEventListener("click", () => {
+        decir(NumeroALetras(number));
+      });
+      if (i == num) {
+        finalizo = true;
+      }
     }
-    elementCaja.appendChild(document.createTextNode(str));
-    const number = i;
-    contarId.appendChild(elementCaja);
-    elementCaja.addEventListener("click", () => {
-      decir(NumeroALetras(number));
-    });
-  }
+  document.getElementById("loading").style.display = "none";
+
+  }, num/10);
+
+
+  await myPromise.then(
+    () => {
+      document.getElementById("loading").style.display = "none";
+    },
+    () => {
+      document.getElementById("loading").style.display = "none";
+    }
+  );
 }
 
 function Abcedario() {
@@ -29,25 +52,24 @@ function Abcedario() {
   abcId.innerHTML = "";
 
   for (var i = 0; i <= 25; i++) {
-
-    const elementCaja = document.createElement('div');
+    const elementCaja = document.createElement("div");
     elementCaja.className = "caja";
-    const code = 'A'.charCodeAt(0);
+    const code = "A".charCodeAt(0);
     let str = String.fromCharCode(code + i);
     elementCaja.appendChild(document.createTextNode(str));
     abcId.appendChild(elementCaja);
     elementCaja.addEventListener("click", () => {
-    decir(str);
+      decir(str);
     });
   }
 }
 
-document.getElementById('hablar').addEventListener("click", () => {
+document.getElementById("hablar").addEventListener("click", () => {
   decir(document.getElementById("texto").value);
 });
 
-document.getElementById('ingresoNumero').addEventListener("click", () => {
-  llenarNumeros(document.getElementById("numero").value);
+document.getElementById("ingresoNumero").addEventListener("click", () => {
+  llenarNumeros(document.getElementById("numero").value).then;
 });
 
 function decir(texto) {
@@ -55,3 +77,5 @@ function decir(texto) {
 }
 
 Abcedario();
+
+export { decir };
